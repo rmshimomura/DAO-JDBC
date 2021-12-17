2c33cafe688db0e14a6bb8ef09f2cdea434008a5
rmshimomura <shimomurarodrigo1@gmail.com>
2021-12-17 18:33:25 -0300

    update implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -30,6 +30,12 @@ public class Program2 {
 		departmentDao.insert(newDepartment);
 		System.out.println("Inserted! New id: " + newDepartment.getId());
 		
+		System.out.println("\n=== TEST 4: update =======");
+		Department dep2 = departmentDao.findById(1);
+		dep2.setName("Food");
+		departmentDao.update(dep2);
+		System.out.println("Update completed");
+		
 		sc.close();
 
 	}
